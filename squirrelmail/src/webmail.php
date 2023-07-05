5e8b0fb2f0a282985ed20ccb87ea157a5040ea39
MrMiget <64145798+MrMiget@users.noreply.github.com>
2023-07-05 18:58:30 -0500

    Add files via upload

---
+++ squirrelmail/src/webmail.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+
+<title>You must be logged in to access this page.</title>
+<style type="text/css">
+<!--
+  /* avoid stupid IE6 bug with frames and scrollbars */
+  body { 
+      voice-family: "\"}\""; 
+      voice-family: inherit; 
+      width: expression(document.documentElement.clientWidth - 30);
+  }
+-->
+</style>
+
+</head>
+
+<BODY TEXT="#000000" BGCOLOR="#FFFFFF" LINK="#0000CC" VLINK="#0000CC" ALINK="#0000CC">
+
+<CENTER><IMG SRC="../images/sm_logo.png" ALT="bigbrainz.com Logo"><BR>
+<SMALL>SquirrelMail version 1.4.3a<BR>
+  By the SquirrelMail Development Team<BR></SMALL>
+<table cellspacing=1 cellpadding=0 bgcolor="#800000" width="70%"><tr><td><TABLE WIDTH="100%" BORDER="0" BGCOLOR="#FFFFFF" ALIGN=CENTER><TR><TD BGCOLOR="#DCDCDC" ALIGN="center"><FONT COLOR="#CC0000"><B>ERROR</B></FONT></TD></TR><TR><TD ALIGN="center">You must be logged in to access this page.</TD></TR><TR><TD BGCOLOR="#DCDCDC" ALIGN="center"><FONT COLOR="#CC0000"><B><a href="/squirrelmail/src/login.php" target="_top">Go to the login page</a></B></FONT></TD></TR></TABLE></td></tr></table></center></body></html>
