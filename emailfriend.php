bdb5c69da38b6597b5b5c3d5060f604ce0b26eac
MrMiget <64145798+MrMiget@users.noreply.github.com>
2023-07-05 18:48:11 -0500

    Add files via upload

---
+++ emailfriend.php
@@ -0,0 +1,134 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link href="/Templates/BigBrainzStyle.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div align="center">
+  </div>
+<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr class="ThinCell">
+    <td width="10" height="10" background="/Assets/Images/TemplateBorders/BBBkgd_03.jpg"><img src="/Assets/Images/TemplateBorders/BBBkgd_01.jpg" width="10" height="10"></td>
+    <td height="10" background="/Assets/Images/TemplateBorders/BBBkgd_03.jpg"><img src="/Assets/Images/TemplateBorders/BBBkgd_02.jpg" width="5" height="10"></td>
+    <td height="10" background="/Assets/Images/TemplateBorders/BBBkgd_03.jpg"><div align="right"><img src="/Assets/Images/TemplateBorders/BBBkgd_05.jpg" width="7" height="10"></div>
+    </td>
+    <td width="13" height="10" background="/Assets/Images/TemplateBorders/BBBkgd_03.jpg"><img src="/Assets/Images/TemplateBorders/BBBkgd_06.jpg" width="13" height="10"></td>
+  </tr>
+  <tr>
+    <td width="10" valign="top" background="/Assets/Images/TemplateBorders/BBBkgd_13.jpg" class="ThinCell"><img src="/Assets/Images/TemplateBorders/BBBkgd_07.jpg" width="10" height="5"></td>
+    <td colspan="2" align="center" valign="middle"><div align="left">
+        <table width="100%" cellspacing="0" cellpadding="0">
+          <tr>
+            <td width="145"><img src="/Assets/Images/BigBrainzLogo60.jpg" width="145" height="60"></td>
+            <td valign="middle"><div align="center">
+                <h1></h1>
+              </div>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </td>
+    <td width="13" valign="top" background="/Assets/Images/TemplateBorders/BBBkgd_15.jpg" class="ThinCell"><img src="/Assets/Images/TemplateBorders/BBBkgd_10.jpg" width="13" height="13"></td>
+  </tr>
+  <tr>
+    <td width="10" height="25%" background="/Assets/Images/TemplateBorders/BBBkgd_13.jpg" class="ThinCell">&nbsp;</td>
+    <td colspan="2" rowspan="2" align="center" valign="top"><br>
+        <table width="100%" border="0" cellpadding="0" cellspacing="0" class="text2">
+          <tr>
+            <td height="215" align="center" valign="middle"><table border='0' cellspacing='4' cellpadding='3' width='100%'>
+                            <script Language="JavaScript">
+<!--
+function formvalidator(theForm)
+{
+  if (theForm.yourname.value == "")
+  {
+    alert("Please enter a value in the field \"Your Name\".");
+    theForm.yourname.focus();
+    return (false);
+  }
+  if (theForm.youremail.value == "")
+  {
+    alert("Please enter a value in the field \"Your Email\".");
+    theForm.youremail.focus();
+    return (false);
+  }
+  if (theForm.friendsemail.value == "")
+  {
+    alert("Please enter a value in the field \"Your Friends Email\".");
+    theForm.friendsemail.focus();
+    return (false);
+  }
+  
+  return (true);
+}
+//-->
+              </script>
+              <form method="POST" action="emailfriend.php" onSubmit="return formvalidator(this)">
+                <input type="hidden" name="posted" value="1">
+                <input type="hidden" name="id" value="">
+                <tr>
+                  <td width="100%" colspan="2" align="center">
+                    <table width='400' border='0' cellpadding='1' cellspacing='1'>
+                      <tr>
+                        <td width="100%" align="center" class="bullet">Let your friends know about this amazing game.<br>Just fill in the details below and click &quot;Send&quot;. </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+                <tr>
+                  <td width="40%" align="right"><font color="#FF0000">*</font>Your Name:</td>
+                  <td><input type="text" name="yourname" size="30">
+                  </td>
+                </tr>
+                <tr>
+                  <td width="40%" align="right"><font color="#FF0000">*</font>Your Email:</td>
+                  <td><input type="text" name="youremail" size="30">
+                  </td>
+                </tr>
+                <tr>
+                  <td width="40%" align="right"><font color="#FF0000">*</font>Your Friends Email:</td>
+                  <td><input type="text" name="friendsemail" size="30">
+                  </td>
+                </tr>
+                <tr>
+                  <td colspan="2" align="center" width="100%">
+                    <table border='0' cellspacing='1' cellpadding='1' width='250'>
+                      <tr>
+                        <td width="100%">&nbsp;<br>
+              Your Comments:<br>
+              <textarea name="yourcomments" cols="30" rows="5"></textarea>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+                <tr>
+                  <td colspan="2" align="center" width="100%"><input type="submit" name="Send" value="Send">
+&nbsp;&nbsp;
+        <input type="button" name="close2" value="Close Window" onClick="javascript:self.close()">
+                  </td>
+                </tr>
+              </form>
+                          </table></td>
+          </tr>
+      </table>
+    </td>
+    <td width="13" rowspan="2" background="/Assets/Images/TemplateBorders/BBBkgd_15.jpg" class="ThinCell">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="25%" background="/Assets/Images/TemplateBorders/BBBkgd_13.jpg" class="ThinCell">&nbsp;</td>
+  </tr>
+  <tr class="ThinCell">
+    <td width="10" height="20"><img src="/Assets/Images/TemplateBorders/BBBkgd_17.jpg" width="10" height="20"></td>
+    <td height="20" background="/Assets/Images/TemplateBorders/BBBkgd_20.jpg"><img src="/Assets/Images/TemplateBorders/BBBkgd_18.jpg" width="10" height="20"></td>
+    <td width="13" height="20" align="right" valign="bottom" background="/Assets/Images/TemplateBorders/BBBkgd_20.jpg" bgcolor="#FFFFFF"><div align="right"><img src="/Assets/Images/TemplateBorders/BBBkgd_22.jpg" width="7" height="20"></div>
+    </td>
+    <td width="13" height="20"><img src="/Assets/Images/TemplateBorders/BBBkgd_23.jpg" width="13" height="20"></td>
+  </tr>
+</table>
+<img src="/Assets/Images/spacer.gif" width="10" height="1">
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,244 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/BigBrainzTemplate2014.dwt" codeOutsideHTMLIsLocked="false" --> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+<!-- InstanceBeginEditable name="doctitle" -->
+
+<title>Big Brainz Multiplication Tables</title>
+
+<!-- InstanceEndEditable -->
+
+<link href="/Templates/BigBrainzStyle2014.css" rel="stylesheet" type="text/css">
+<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+<!--[if !IE 7]><style type="text/css">#wrap {display:table;height:100%}</style><![endif]-->
+<script language="JavaScript" type="text/javascript" src="/Assets/Scripts/PopUpWindow.txt"></script>
+
+
+
+<!-- InstanceBeginEditable name="Desciption and Keywords" -->
+
+<link rel="canonical" href="http://www.bigbrainz.com"/>
+
+<meta name="description" content="The most powerful program ever developed to master multiplication tables.">
+
+<meta name="keywords" content="Multiplication, Multiplication Games, Times Tables, Multiplication Table">
+
+<!-- InstanceEndEditable -->
+
+<!-- Google Analytics Code -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-94233-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+<!-- End Google Analaytics Code -->
+
+</head> 
+<body> 
+  <div id="pagewrapper">
+	<div id="header"> 
+		<h1><a href="http://www.bigbrainz.com">Multiplication Games</a></h1> 
+		<ul id="topMenu"> 
+			<li><a href="/Store.php">Store</a></li>
+			<li><a href="/Downloads.html">Downloads<br>(Home-Use)</a></li>
+			<li><a href="/Schools.php">Schools</a></li>
+			<li><a href="/Support.php">Support</a></li>
+			<li><a href="/games/launcher/app.html">Reports</a></li>
+			<li><a href=""></a></li>			
+			<li><a href="/blog/">Blog</a></li>
+			<li><a href="/cart.php">Checkout</a></li>
+		</ul> 
+	</div> 
+
+
+
+	<div id="main">
+		<div id="boxmain">
+<!-- InstanceBeginEditable name="Edit Region 1" -->	
+
+<h1>Final Chance to Purchase a Home License!</h1>
+<p align="left" class="text">Big Brainz is thrilled to announce that we have been acquired by <a href="http://www.imaginelearning.com/">Imagine Learning</a>, one of the top educational software companies in the country. Imagine Learning shares our vision for revolutionizing educational software and is an established expert on  language and literacy. This new relationship allows us to serve far more students and greatly expand the product we can offer. However, the intense focus we will have on the school market  means we can no longer serve home users. We know many potential at-home customers have been planning on purchasing or upgrading. If this is your situation, we will allow you to purchase a home license through Feb 5th. Thereafter you will need to work with your school to purchase a site license so that ALL students can benefit from this incredible program. </p>
+<p align="left" class="text">&nbsp;</p>
+<p align="left" class="text">Many thanks to all our fantastic fans and users over the years who have allowed us to help so many students master their core math skills. We will continue to support paid users via email and  our<a href="http://bigbrainz.com/Support.php"> support page</a>.</p>
+<p align="left" class="text">&nbsp;</p>
+<!-- InstanceEndEditable -->
+	  	</div>
+		
+	  <!-- InstanceBeginEditable name="EditRegion4" -->
+
+		<div id="videoColumnLeft">
+
+		  <p>
+<script>
+var image = new Image();
+image.src = "http://youtube.com/favicon.ico";
+image.onload = function(){
+	// The user can access youtube
+	var youtube_html = '<iframe width="560" height="315" src="//www.youtube.com/embed/ud4lQC39pn0" frameborder="0" controls=2 allowfullscreen></iframe>';
+	document.getElementById('youtube').innerHTML = youtube_html;
+};
+image.onerror = function(){
+// The user can't access youtube
+	var youtube_html = '<a href="http://s3.amazonaws.com/BigBrainz/Quick-Start-Video-2014.mp4" title="TA_30Sec" target="_new" rel="#voverlay"><img src="Assets/Images/Quick-Start-Video-Play-Image.png" alt="TA_30Sec" width="560" height="315" border="0" /></a>';
+	document.getElementById('youtube').innerHTML = youtube_html;
+
+};
+
+</script>
+<div id = "youtube">:)</div>
+		  </p>
+
+  	  </div>
+
+	  	
+
+		<div id="videoColumnRight">
+
+		  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+
+            <tr>
+
+              <td><div class="homeUse" id="homeUse";>Home Use </div></td>
+
+              <td><div class="schoolUse" id="schoolUse"><a href="/Schools.php">School Use </a></div></td>
+
+            </tr>
+
+            <tr class="smalltext">
+              <td id="borderDrawTL">&nbsp;</td>
+              <td id="borderDrawTR">&nbsp;</td>
+            </tr>
+            <tr>
+              <td colspan="2" align="center" id="borderDrawMiddle"><a href="Store.php"><img src="Assets/Images/Buttons/Shop-Now_210.png" width="210" height="50" alt="Shop Now" /></a></td>
+            </tr>
+            <tr>
+
+              <td id="borderDrawTL"><div align="center"><a href="/Multiplication.php"><img src="Assets/Images/Buttons/Product-Icon-Mult-Red-100.png" alt="Multiplication" width="100" height="100" border="0" /></a><br />
+              </div></td>
+
+              <td id="borderDrawMiddleRight"><div align="center"><a href="/Division.php"><img src="Assets/Images/Buttons/Product-Icon-Plain-Div-Yellow-100.png" alt="Multiplication" width="100" height="100" border="0" /></a><br />
+              </div></td>
+
+            </tr>
+
+            <tr>
+              <td id="borderDrawBL"><div align="center"><a href="/Addition.php"><span class="text14Bold"><img src="Assets/Images/Buttons/Product-Icon-Add-BlueMatch-100-New.png" alt="Multiplication" width="100" height="100" border="0" /></span></a><br />
+              </div></td>
+              <td id="borderDrawBR"><div align="center"><a href="/Subtraction.php"><span class="text14Bold"><img src="Assets/Images/Buttons/Product-Icon-Sub-BlueMatchLight-100-New.png" alt="Multiplication" width="100" height="100" border="0" /></span></a><br />
+              </div></td>
+            </tr>
+          </table>
+
+  	  </div>
+
+	  <!-- InstanceEndEditable -->
+		
+		<!-- InstanceBeginEditable name="EditRegion5" -->
+
+		<div id="box2">
+
+		  <table width="100%" border="0" cellspacing="0" bordercolor="004080">
+
+            <tr>
+
+              <td width="20%" align="center" valign="middle" no-repeat="no-repeat"><div align="center">
+
+                  <p class="NoTile"><a href="Guarantee.php"><img src="Assets/Images/SquareRed_MoneyBack.jpg" alt="Big Brainz Phone #" width="157" height="82" /></a></p>
+
+              </div></td>
+
+              <td width="20%" align="center" valign="middle" no-repeat="no-repeat"><div align="center">
+
+                  <p class="NoTile"><img src="Assets/Images/SquareBlue_CallUs.jpg" alt="Big Brainz Phone #" width="157" height="82" /></p>
+
+              </div></td>
+
+              <td width="20%"><div align="center" class="BigBrainzStyle"><img src="Assets/Images/TALogo3_156.jpg" alt="Timez Attack Logo" width="156" height="106" /></div></td>
+
+              <td width="20%" align="center" valign="middle" no-repeat="no-repeat"><div align="center">
+
+                  <p class="NoTile"><img src="Assets/Images/SquareBlue_Million.jpg" alt="Big Brainz Phone #" width="157" height="82" /></p>
+
+              </div></td>
+
+              <td width="20%" align="center" valign="middle" no-repeat="no-repeat"><div align="center">
+
+                  <p class="NoTile">
+                  <a href="http://www.securitymetrics.com/site_certificate.adp?s=97%2e75%2e160%2e82&amp;i=963526" target="_blank" >
+<img src="http://www.securitymetrics.com/images/sm_ccsafe_whborder.gif" alt="SecurityMetrics for PCI Compliance, QSA, IDS, Penetration Testing, Forensics, and Vulnerability Assessment" border="0"> </a></p>
+
+              </div></td>
+
+            </tr>
+
+          </table>
+
+	  	</div>
+
+		<!-- InstanceEndEditable -->
+	</div>
+	  
+</div>
+<div id="footer">
+		<ul id="bottomMenu"> 
+			<li><a href="/Contact.php">Contact</a></li> 
+			<li><a href="/reviews/web/">Reviews</a></li> 
+			<li><a href="/Articles">Articles</a></li> 
+			
+			<li><a href="/LinkToUs/LinkToUs.html">Link<br /> 
+		    To Us</a></li> 
+			<li><a href="/affiliate1.php">Affiliates</a></li> 
+			<li><a href="/Shipping.php">Shipping</a></li> 
+			<li><a href="/Guarantee.php">Guarantee</a></li> 
+			<li><a href="/TermsOfUse.php">Terms <br />
+		    of Use</a></li> 
+			<li><a href="/Legal.php">Legal</a></li> 
+			<li><a href="/About.php">About</a></li> 
+			<li><a href="/orderstatus.php">My Orders </a></li>
+		</ul> 
+<div id="copyright">&copy; 2004 - 2014 Big Brainz, Inc.</div>
+</div>
+
+<!-- NEW Google Analytics Tracking Code-->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-94233-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+<!-- END Google Analytics Tracking Code-->
+
+<!-- BEGIN AdRoll Tracking Code-->
+<script type="text/javascript">
+adroll_adv_id = "X36G6FX3ABDDTKTJBOZMWT";
+adroll_pix_id = "UFCWAPQTOJBVNAE2Q4G3XI";
+(function () {
+var oldonload = window.onload;
+window.onload = function(){
+   __adroll_loaded=true;
+   var scr = document.createElement("script");
+   var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+   scr.setAttribute('async', 'true');
+   scr.type = "text/javascript";
+   scr.src = host + "/j/roundtrip.js";
+   ((document.getElementsByTagName('head') || [null])[0] ||
+    document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+   if(oldonload){oldonload()}};
+}());
+</script>
+<!-- END AdRoll Tracking Code-->
+
+</body>
+
+<!-- InstanceEnd --></html>
