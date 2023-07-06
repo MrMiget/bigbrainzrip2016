6d9d3a3d147a9a84b9b83a3389cc6d86f3140e34
MrMiget <64145798+MrMiget@users.noreply.github.com>
2023-07-05 20:06:25 -0500

    Add files via upload

---
+++ Assets/Remote.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/BigBrainzTemplate2013.dwt" codeOutsideHTMLIsLocked="false" --> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Remote Install Instructions</title>
+<!-- InstanceEndEditable -->
+
+<link href="/Templates/BigBrainzStyle2011.css" rel="stylesheet" type="text/css">
+<!--[if !IE 7]><style type="text/css">#wrap {display:table;height:100%}</style><![endif]-->
+<script language="JavaScript" type="text/javascript" src="/Assets/Scripts/PopUpWindow.txt"></script>
+
+
+
+<!-- InstanceBeginEditable name="Desciption and Keywords" -->
+
+<!-- InstanceEndEditable -->
+
+<!-- Google Analytics Code -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-94233-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+<!-- End Google Analaytics Code -->
+
+</head> 
+<body> 
+  <div id="pagewrapper">
+	<div id="header"> 
+		<h1><a href="http://www.bigbrainz.com">Multiplication Games</a></h1> 
+		<ul id="topMenu"> 
+			<li><a href="/index.php">Products</a></li>
+			<li><a href="/Downloads.html">Downloads<br>(Home-Use)</a></li>
+			<li><a href="/Schools.php">Schools</a></li>
+			<li><a href="/Support.php">Support</a></li>
+			<li><a href="/games/launcher/app.html">Reports</a></li>
+			<li><a href=""></a></li>			
+			<li><a href="/blog/">Blog</a></li>
+			<li><a href="/cart.php">Checkout</a></li>
+		</ul> 
+	</div> 
+
+<!-- Begin Facebook Code -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<!-- End Facebook Code -->
+
+	<div id="main">
+		<div id="boxmain">
+<!-- InstanceBeginEditable name="Edit Region 1" -->
+      <div align="center">
+        <table width="100%" height="100%" border="0" cellpadding="0" cellspacing="0">
+          <tr class="BigBrainzStyle">
+            <td><div align="center">
+              <p class="Headline">Remote Install Instructions</p>
+              <p class="notice3">(For the very <a href="../Implementation.php">latest version</a> only!!)</p>
+              <p class="notice3"><img src="Images/Scratch1_100.jpg" width="74" height="100" /></p>
+              <p>&nbsp;</p>
+              <p><span class="title">Option #1&#8212;Super Simple</span><br />
+                <span class="smalltext">(simplest)</span></p>
+              <p class="text"><a href="http://bigbrainz.com/Schools.php">Download</a> and run the installer on each computer</p>
+              <p><br />
+&nbsp;              </p>
+              <p><span class="title">Option #2</span><span class="title">&#8212;Simple</span><br />
+                <span class="smalltext">(Still super simple--better for slow connections) </span></p>
+              <p class="text"><a href="http://bigbrainz.com/Schools.php">Download</a> the installer once to a USB stick then walk it around to each computer and run it.</p>
+              <p><br />
+&nbsp;              </p>
+              <p class="title">Option #3&#8212;Clever</p>
+              <p class="text">1<span class="text12Bold">. </span><a href="http://bigbrainz.com/Schools.php"> Download</a> and run the installer on one computer.<br />
+  &nbsp;                </p>
+              <p class="text"><span class="text">2.</span> <span class="text12Bold">Windows</span>: Copy &quot;BigBrainz&quot; folder(s) to other computers<br />
+                <span class="text12Bold">Mac</span>: Copy BigBrainz.app and Big Brainz Folder:&nbsp;                  <br />
+                  <img src="Images/Remote-Install-Folder-Image.png" width="524" height="225" alt="Remote Install Folder Locations" />                <br />
+&nbsp;                </p>
+              <p class="text"><span class="text12Bold">3.</span> Be sure to preserve full read/write access to folders.<br />
+  &nbsp;                </p>
+              <p class="text"><span class="text12Bold">4.</span> Create shortcuts to BigBrainz.app or BigBrainz.exe (in ProgramData/BigBrainz).<br />
+  &nbsp;                </p>
+              <p class="text">That's it--you're done!<br />
+  &nbsp;                </p>
+            </div></td>
+          </tr>
+        </table>
+      </div>
+    <!-- InstanceEndEditable -->
+	  	</div>
+		
+	  <!-- InstanceBeginEditable name="EditRegion4" -->
+		<!-- InstanceEndEditable -->
+		
+		<!-- InstanceBeginEditable name="EditRegion5" -->
+		<!-- InstanceEndEditable -->
+	</div>
+	  
+</div>
+<div id="footer">
+		<ul id="bottomMenu"> 
+			<li><a href="/Contact.php">Contact</a></li> 
+			<li><a href="/Reviews.html">Reviews</a></li> 
+			<li><a href="/Articles">Articles</a></li> 
+			
+			<li><a href="/LinkToUs/LinkToUs.html">Link<br /> 
+		    To Us</a></li> 
+			<li><a href="/affiliate1.php">Affiliates</a></li> 
+			<li><a href="/Shipping.php">Shipping</a></li> 
+			<li><a href="/Guarantee.php">Guarantee</a></li> 
+			<li><a href="/TermsOfUse.php">Terms <br />
+		    of Use</a></li> 
+			<li><a href="/Legal.php">Legal</a></li> 
+			<li><a href="/About.php">About</a></li> 
+			<li><a href="/orderstatus.php">My Orders </a></li>
+		</ul> 
+<div id="copyright">&copy; 2004 - 2013 Big Brainz, Inc.</div>
+</div>
+
+<!-- NEW Google Analytics Tracking Code-->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-94233-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+<!-- END Google Analytics Tracking Code-->
+
+<!-- BEGIN AdRoll Tracking Code-->
+<script type="text/javascript">
+adroll_adv_id = "X36G6FX3ABDDTKTJBOZMWT";
+adroll_pix_id = "UFCWAPQTOJBVNAE2Q4G3XI";
+(function () {
+var oldonload = window.onload;
+window.onload = function(){
+   __adroll_loaded=true;
+   var scr = document.createElement("script");
+   var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+   scr.setAttribute('async', 'true');
+   scr.type = "text/javascript";
+   scr.src = host + "/j/roundtrip.js";
+   ((document.getElementsByTagName('head') || [null])[0] ||
+    document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+   if(oldonload){oldonload()}};
+}());
+</script>
+<!-- END AdRoll Tracking Code-->
+
+</body>
+
+<!-- InstanceEnd --></html>
